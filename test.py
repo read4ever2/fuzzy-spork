cd8dbae163bc608cd5838faabd6192542305e252
read4ever2 <62565368+read4ever2@users.noreply.github.com>
2021-03-31 15:30:05 -0500

    Update test.py

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ import secrets
 
 def main():
     for _ in range(10):
-        print(secrets.randbelow(1_000_000))
+        print(f'{secrets.randbelow(1_000_000)}'.zfill(6))
         
 if __name__ == "__main__":
     main()
